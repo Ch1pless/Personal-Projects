77e19d4a79ff1830ef06f61811cda8415c777493
Ch1pless <kylecochran123@gmail.com>
2018-12-20 19:02:23 -0500

    Update

---
+++ python/beginnerPrograms/divisors.py
@@ -0,0 +1,8 @@
+# Made by Ch1pless
+
+number = int(input("What number do you want the divisors of? "))
+
+div = [num for num in range(1,number+1) if number % num == 0]
+
+print("List of Divisors of " + str(number) + ": " + str(div))
+print("*"*20)
---
+++ python/beginnerPrograms/listOverlap.py
@@ -0,0 +1,14 @@
+# Made by Ch1pless
+
+import random
+
+a = [random.randrange(0,20,1) for i in range(random.randrange(5,20,1))]
+b = [random.randrange(0,20,1) for i in range(random.randrange(5,20,1))]
+c = [a[ind] for ind in range(len(a)) if a[ind] in b and a[ind] not in a[slice(0,ind,1)]]
+
+print("List 1: " + str(a) + '\n' + "List 2:" + str(b))
+if len(c) == 0 :
+	print("No matching numbers.")
+else :
+	print("Matching Numbers: " + str(c))
+print("*"*20)
